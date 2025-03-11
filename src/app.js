9a2555e10ff8844df0d5affaa4e9058ccf13b1c0
joelAntonioDev <joellucasxekfrencys2017@gmail.com>
2025-03-11 01:20:08 +0100

    Changing the cors endpoint

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const taskRoutes = require('./routes/taskRoutes');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "https://to-do-deploy-two.vercel.app/",
-    methods: "GET,POST,PUT,DELETE",
+app.use(cors({
+    origin: "https://to-do-deploy-two.vercel.app", // URL correta do front-end
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: "Content-Type, Authorization",
     credentials: true
- }));
+}));
 
 app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
