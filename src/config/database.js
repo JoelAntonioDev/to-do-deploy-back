59140bd469dd633583e54e7227867405ed211d63
joelAntonioDev <joellucasxekfrencys2017@gmail.com>
2025-03-11 02:42:17 +0100

    Changing the cors endpoint

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,16 @@
 const mysql = require("mysql2/promise");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     port: process.env.DATABASE_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-connection.connect(err => {
-    if (err) {
-        console.error("❌ Erro ao conectar ao MySQL:", err);
-        return;
-    }
-    console.log("✅ Conectado ao MySQL do InfinityFree!");
-});
+console.log("✅ Conexão com MySQL configurada!");
 
-module.exports = connection;
+module.exports = pool;
