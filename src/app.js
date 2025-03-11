e99fa4224c6c660eee6b064e799076eaa6c5af90
joelAntonioDev <joellucasxekfrencys2017@gmail.com>
2025-03-11 01:13:25 +0100

    Changing the cors endpoint

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const taskRoutes = require('./routes/taskRoutes');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "https://to-do-deploy-two.vercel.app/" }));
+app.use(cors({ origin: "https://to-do-deploy-two.vercel.app/",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+ }));
 
 app.use('/users', userRoutes);
 app.use('/tasks', taskRoutes);
